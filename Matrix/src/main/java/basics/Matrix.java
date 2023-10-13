367a128cf66fcc5c289f15c063fd63b4ad0e8f9e
Pierre V <6772696d@gmail.com>
2023-10-13 14:27:57 +0200

    Finish Matrix and add ASCIIDecoder

--- Matrix/src/main/java/basics/Matrix.java
+++ Matrix/src/main/java/basics/Matrix.java
@@ -4,17 +4,26 @@ public class Matrix {
 
     /**
      * Create a matrix from a String.
-     *
+     * <p>
      * The string if formatted as follow:
-     *  - Each row of the matrix is separated by a newline
-     *    character \n
-     *  - Each element of the rows are separated by a space
+     * - Each row of the matrix is separated by a newline
+     * character \n
+     * - Each element of the rows are separated by a space
      *
-     *  @param s The input String
-     *  @return The matrix represented by the String
+     * @param s The input String
+     * @return The matrix represented by the String
      */
     public static int[][] buildFrom(String s) {
-         return null;
+        String[] row = s.split("\n");
+        int[][] result = new int[row.length][];
+        for (int i = 0; i < row.length; i++) {
+            String[] numb = row[i].split(" ");
+            result[i] = new int[numb.length];
+            for (int j = 0; j < numb.length; j++) {
+                result[i][j] = Integer.parseInt(numb[j]);
+            }
+        }
+        return result;
     }
 
 
@@ -25,7 +34,13 @@ public class Matrix {
      * @return The sum of the element in matrix
      */
     public static int sum(int[][] matrix) {
-         return 0;
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                sum += matrix[i][j];
+            }
+        }
+        return sum;
     }
 
     /**
@@ -35,7 +50,14 @@ public class Matrix {
      * @return A new matrix that is the transpose of matrix
      */
     public static int[][] transpose(int[][] matrix) {
-         return null;
+        int[][] transpose = new int[matrix[0].length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        return transpose;
     }
 
     /**
@@ -46,6 +68,18 @@ public class Matrix {
      * @return The n x k matrix product of matrix1 and matrix2
      */
     public static int[][] product(int[][] matrix1, int[][] matrix2) {
-         return null;
+        int n = matrix1.length;
+        int m = matrix1[0].length;
+        int k = matrix2[0].length;
+
+        int[][] matrix3 = new int[n][k];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k; j++) {
+                for (int h = 0; h < m; h++) {
+                    matrix3[i][j] += matrix1[i][h] * matrix2[h][j];
+                }
+            }
+        }
+        return matrix3;
     }
 }
--- Matrix/src/main/java/basics/testing.java
+++ Matrix/src/main/java/basics/testing.java
@@ -1,4 +1,52 @@
 package basics;
 
+import java.lang.reflect.Array;
+
 public class testing {
+    public static void main(String[] args) {
+        String s = "1 2 3.1\n3 4 5";
+        String[] lines = s.trim().split("\n");
+        int rows = lines.length;
+        int cols = lines[0].split(" ").length;
+        int[][] matrix = new int[rows][cols];
+        // System.out.println(rows + " " + cols);
+        System.out.println(lines[0]);
+        System.out.println(lines[1]);
+
+        for (int i = 0; i < lines.length; i++) {
+
+            for (int j = 0; j < lines[i].split(" ").length; j++) {
+                try {
+                    matrix[i][j] = Integer.parseInt(lines[i].split(" ")[j]);
+                } catch (NumberFormatException e) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        System.out.println(matrix[0][0] + " " + matrix[0][1] + " " + matrix[0][2]);
+        int[][] matrix1 = {{1, 2, 3}, {4, 5, 6}};
+        int[][] matrix2 = {{2}, {4}, {6}};
+
+        int[][] product = product(matrix1, matrix2);
+        System.out.println(product[0][0]);
+
+
+    }
+
+    public static int[][] product(int[][] matrix1, int[][] matrix2) {
+        int n = matrix1.length;
+        int m = matrix1[0].length;
+        int k = matrix2[0].length;
+
+        int[][] matrix3 = new int[n][k];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k; j++) {
+                for (int h = 0; h < m; h++) {
+                    matrix3[i][j] += matrix1[i][h] * matrix2[h][j];
+                }
+            }
+        }
+        return matrix3;
+    }
 }
